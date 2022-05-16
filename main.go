ac8b460086b7ae9b140812b643c2d25ea5c98498
louxudong1993@163.com <louxudong1993@163.com>
2022-05-16 23:16:28 +0800

    test post and get

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,5 +13,16 @@ func main() {
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "hello,work!")
 	})
+	r.POST("/testPost", getting)
+	r.PUT("/testPut", putting)
 	r.Run(":3510")
+}
+
+func getting(ctx *gin.Context) {
+	fmt.Println("收到了Post请求")
+	ctx.String(http.StatusOK, "收到了Post请求")
+}
+func putting(ctx *gin.Context) {
+	fmt.Println("收到了Put请求")
+	ctx.String(http.StatusOK, "收到了Put请求")
 }
