981534e6e3addd476374e91bb1c2f1577640448f
楼旭栋 <louxudong1993@163.com>
2022-05-16 22:56:13 +0800

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello,work!")
+	})
+	r.Run(":3510")
+}
